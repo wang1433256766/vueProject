bf4c04eb38f8d4b952c86101e9f889ad7c9a6e94
wangsir <1433256766@qq.com>
2017-11-28 15:36:32 +0800

    增加修改密码和用户名的功能

--- src/components/MyAccount.vue
+++ src/components/MyAccount.vue
@@ -12,7 +12,7 @@
 					    <template slot="prepend">用户名</template>
 					</el-input>
 				</div>
-				<div style="margin-top: 15px;">
+				<!-- <div style="margin-top: 15px;">
 					<el-input placeholder="" v-model="inputMyAccount.department" disabled>
 					    <template slot="prepend">单位</template>
 					</el-input>
@@ -21,7 +21,7 @@
 					<el-input placeholder="" v-model="inputMyAccount.tel" disabled>
 					    <template slot="prepend">联系方式</template>
 					</el-input>
-				</div>
+				</div> -->
 		    </el-tab-pane>
 		    <el-tab-pane label="资料修改" name="second">
 		    	<div style="margin-top: 15px;">
@@ -34,7 +34,7 @@
 					    <template slot="prepend">用户名</template>
 					</el-input>
 				</div>
-				<div style="margin-top: 15px;">
+				<!-- <div style="margin-top: 15px;">
 					<el-input placeholder="请输入单位" v-model="inputUpt.department">
 					    <template slot="prepend">单位</template>
 					</el-input>
@@ -43,24 +43,24 @@
 					<el-input placeholder="请输入联系方式" v-model="inputUpt.tel">
 					    <template slot="prepend">联系方式</template>
 					</el-input>
-				</div>
+				</div> -->
 				<div style="margin-top: 25px;text-align:center;">
 					<el-button type="primary" @click="updateMyInfo">确认修改</el-button>
 				</div>
 		    </el-tab-pane>
 		    <el-tab-pane label="密码修改" name="third">
-		    	<div style="margin-top: 15px;">
+		    	<!-- <div style="margin-top: 15px;">
 			    	<el-input placeholder="请输入原密码" v-model="inputPwd.password">
 					    <template slot="prepend">原密码</template>
 					</el-input>
-				</div>
+				</div> -->
 				<div style="margin-top: 15px;">
-					<el-input placeholder="请输入新密码" v-model="inputPwd.newPassword">
+					<el-input type="password" placeholder="请输入新密码" v-model="inputPwd.newPassword">
 					    <template slot="prepend">新密码</template>
 					</el-input>
 				</div>
 				<div style="margin-top: 15px;">
-					<el-input placeholder="请确认密码" v-model="inputPwd.comfirmPassword">
+					<el-input type="password" placeholder="请确认密码" v-model="inputPwd.comfirmPassword">
 					    <template slot="prepend">确认密码</template>
 					</el-input>
 				</div>
@@ -87,18 +87,18 @@ let Base64 = require('js-base64').Base64;
 		        activeName: 'first',
 		        inputMyAccount: {
 		        	email: '',
-		        	username: '',
-		        	department: '',
-		        	tel: ''
+		        	username: ''
+		        	// department: '',
+		        	// tel: ''
 		        },
 		        inputUpt: {
 		        	email: '',
-		        	username: '',
-		        	department: '',
-		        	tel: ''
+		        	username: ''
+		        	// department: '',
+		        	// tel: ''
 		        },
 		        inputPwd: {
-		        	password: '',
+		        	//password: '',
 		        	newPassword: '',
 		        	comfirmPassword: ''
 		        }
@@ -132,11 +132,88 @@ let Base64 = require('js-base64').Base64;
 	      	},
 	      	//修改个人信息
 	      	updateMyInfo(){
-
+	      		if(!this.inputUpt.username){
+	      			this.$message({
+		    			showClose: true,
+			          	message: '请填写用户名！',
+			          	type: 'warning'
+			        });
+		    		return false;
+	      		}
+	      		let params = {
+	      			id: this.uid,
+	      			username: this.inputUpt.username
+	      		}
+	      		api.UpdateUserStatus(params)
+	      			.then(res => {
+	      				if(res.status == 1){
+		    				this.$notify({
+					          	title: '',
+					          	message: '用户名修改成功',
+					          	type: 'success'
+					        });
+					        this.getUserInfo()
+		    			}else{
+		    				this.$notify.error({
+						        title: '失败',
+						        message: '用户名修改失败'
+					        });
+		    			}
+	      			})
+	      			.catch(error => {
+	      				console.log(error);
+	      			})
 	      	},
 	      	//修改密码
 	      	updatePwd(){
-
+	      		if(!this.inputPwd.newPassword){
+	      			this.$message({
+		    			showClose: true,
+			          	message: '请填写要修改的密码及确认密码',
+			          	type: 'warning'
+			        });
+		    		return false;
+	      		}
+	      		if(this.inputPwd.newPassword.length<6){
+	      			this.$message({
+		    			showClose: true,
+			          	message: '密码请超过六位',
+			          	type: 'warning'
+			        });
+		    		return false;
+	      		}
+	      		if(this.inputPwd.newPassword != this.inputPwd.comfirmPassword){
+	      			this.$message({
+		    			showClose: true,
+			          	message: '两次密码不一致',
+			          	type: 'warning'
+			        });
+		    		return false;
+	      		}
+	      		let params = {
+	      			id: this.uid,
+	      			pwd: this.inputPwd.newPassword
+	      		}
+	      		api.UpdateUserStatus(params)
+	      			.then(res => {
+	      				if(res.status == 1){
+		    				this.$notify({
+					          	title: '',
+					          	message: '密码修改成功',
+					          	type: 'success'
+					        });
+					        this.inputPwd.newPassword = '';
+					        this.inputPwd.comfirmPassword = '';
+		    			}else{
+		    				this.$notify.error({
+						        title: '失败',
+						        message: '密码修改失败'
+					        });
+		    			}
+	      			})
+	      			.catch(error => {
+	      				console.log(error);
+	      			})
 	      	}
 	    }
 	}
